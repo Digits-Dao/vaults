6aee3f88dbeb726dc4e4dfbe15bc46e8b6dea991
pbnather <@pbnather>
2022-11-08 16:27:36 +0100

    Finish docs

--- contracts/RedemptionHelper.sol
+++ contracts/RedemptionHelper.sol
@@ -17,12 +17,19 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
     /* ============ Structures ============ */
 
     struct Redemption {
+        // Time after which redemption can be activated.
         uint256 redemptionTime;
+        // Time until users can (un)register their tokens.
         uint256 registrationEndTime;
+        // Pending tokens to redeem.
         uint256 pending;
+        // Redemtpion value of tokens in 'token' after 'fee'.
         uint256 price;
+        // Fee taken (max 10%).
         uint256 fee;
+        // Token address used for redemption.
         IERC20 token;
+        // If users can redeem.
         bool active;
     }
 
@@ -38,6 +45,9 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
 
     /* ============ Initializer ============ */
 
+    /**
+     * @notice Initializer function called by factory.
+     */
     function initialize(address owner_, address admin_) external initializer {
         require(owner_ != address(0));
         require(admin_ != address(0));
@@ -60,24 +70,54 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
 
     /* ============ External Owner Functions ============ */
 
+    /**
+     * @notice Set default interval between redemptions.
+     *
+     * @dev It can be overriden by owner on 'activateRedemption'.
+     *
+     * @param interval New redemption interval (in seconds).
+     */
     function setRedemptionInterval(uint256 interval) external onlyOwner {
         require(interval > _preparationTime);
         _redemptionInterval = interval;
         emit RedemptionIntervalSet(interval);
     }
 
+    /**
+     * @notice Set preparation time for next redemption.
+     *
+     * @dev Preparation time allows users to register/unregister.
+     *
+     * @param preparationTime New preparation time (in seconds).
+     */
     function setPreparationTime(uint256 preparationTime) external onlyOwner {
         require(preparationTime < _redemptionInterval);
         _preparationTime = preparationTime;
         emit PreparationTimeSet(preparationTime);
     }
 
+    /**
+     * @notice Set redemption token.
+     *
+     * @dev Current contract supports only stablecoins.
+     *
+     * @param token Address of the redemption token.
+     */
     function setRedemptionToken(IERC20 token) external onlyOwner {
         require(token != IERC20(address(0)));
         _redemptionToken = token;
         emit RedemptionTokenSet(token);
     }
 
+    /**
+     * @notice Create first redemption.
+     *
+     * @dev It can only be called once, before any redemptions.
+     * @dev 'nextRedemptionExactTime' greater than 0 ignores '_redemptionInterval'.
+     *
+     * @param nextRedemptionExactTime Exact time of the new redemption (ignored if 0).
+     * @param fee Fee for the new redemption (max 1000 = 10%).
+     */
     function initializeRedemptions(uint256 nextRedemptionExactTime, uint256 fee)
         external
         onlyOwner
@@ -91,6 +131,16 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
         _addNewRedemption(nextRedemptionExactTime, fee);
     }
 
+    /**
+     * @notice Activate redemption, burn registered vault tokens, transfer redemption tokens
+     * from owner to the contract, allow users to redeem.
+     *
+     * @dev Calling this function will create next redemption object.
+     * @dev 'nextRedemptionExactTime' greater than 0 ignores '_redemptionInterval'.
+     *
+     * @param nextRedemptionExactTime Exact time of the new redemption (ignored if 0).
+     * @param fee Fee for the new redemption (max 1000 = 10%).
+     */
     function activateRedemption(uint256 nextRedemptionExactTime, uint256 fee)
         external
         onlyOwner
@@ -166,6 +216,11 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
 
     /* ============ External Functions ============ */
 
+    /**
+     * @notice Reedem redemption tokens.
+     *
+     * @param claims List of redemption indexes to claim from.
+     */
     function redeem(uint256[] memory claims) external {
         for (uint256 i = 0; i < claims.length; i++) {
             uint256 index = claims[i];
@@ -185,6 +240,13 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
         }
     }
 
+    /**
+     * @notice Register vault tokens for the current redemption.
+     *
+     * @dev Only works if current redemtpion 'registrationTimeEnd' hasn't passed yet.
+     *
+     * @param amount Amount of vault tokens to register.
+     */
     function register(uint256 amount) external {
         uint256 index = redemptions.length - 1;
         Redemption storage redemption = redemptions[index];
@@ -202,6 +264,13 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
         emit Registered(index, msg.sender, amount, redemption.token);
     }
 
+    /**
+     * @notice Unegister vault tokens for the current redemption.
+     *
+     * @dev Only works if current redemtpion 'registrationTimeEnd' hasn't passed yet.
+     *
+     * @param amount Amount of vault tokens to register.
+     */
     function unregister(uint256 amount) external {
         uint256 index = redemptions.length - 1;
         Redemption storage redemption = redemptions[index];
@@ -220,4 +289,118 @@ contract RedemptionHelper is IRedemptionHelper, Ownable, Initializable {
     }
 
     /* ============ External View Functions ============ */
+
+    /**
+     * @notice Returns amount of vault tokens registered by the user for given redemption.
+     *
+     * @param _user Address of the vault user.
+     * @param _redemptionIndex Index of the redemption.
+     *
+     * @return amount Amount of tokens registered by the user.
+     * @return token Address of the redemption token.
+     */
+    function getRegisteredAmount(address _user, uint256 _redemptionIndex)
+        external
+        view
+        returns (uint256 amount, IERC20 token)
+    {
+        require(_redemptionIndex < redemptions.length);
+        amount = userClaims[_user][_redemptionIndex];
+        token = redemptions[_redemptionIndex].token;
+    }
+
+    /**
+     * @notice Returns number of redemptions in the list.
+     *
+     * @return length Number of redemptions (length of redemptions list).
+     */
+    function getRedemptionsLength() external view returns (uint256 length) {
+        return redemptions.length;
+    }
+
+    /**
+     * @notice Returns all information of the given redemption.
+     *
+     * @param _redemptionIndex Index of the redemption.
+     *
+     * @return redemptionTime Time after which redemption can be activated.
+     * @return registrationEndTime Time until users can (un)register their tokens.
+     * @return pending Pending tokens to redeem.
+     * @return price Redemtpion value of tokens in 'token' after 'fee'.
+     * @return fee Fee taken (max 10%).
+     * @return token Token address used for redemption.
+     * @return active If users can redeem.
+     */
+    function getRedemption(uint256 _redemptionIndex)
+        external
+        view
+        returns (
+            uint256 redemptionTime,
+            uint256 registrationEndTime,
+            uint256 pending,
+            uint256 price,
+            uint256 fee,
+            IERC20 token,
+            bool active
+        )
+    {
+        require(_redemptionIndex < redemptions.length);
+        Redemption storage redemption = redemptions[_redemptionIndex];
+        redemptionTime = redemption.redemptionTime;
+        registrationEndTime = redemption.registrationEndTime;
+        pending = redemption.pending;
+        price = redemption.price;
+        fee = redemption.fee;
+        token = redemption.token;
+        active = redemption.active;
+    }
+
+    /**
+     * @notice Returns next redemption fee.
+     *
+     * @return nextRedemptionFee Next redemption fee.
+     */
+    function getNextRedemptionFee()
+        external
+        view
+        returns (uint256 nextRedemptionFee)
+    {
+        uint256 length = redemptions.length;
+        require(length > 0);
+        nextRedemptionFee = redemptions[length - 1].fee;
+    }
+
+    /**
+     * @notice Returns next redemption time.
+     *
+     * @return nextRedemptionTime Time after which redemption can be activated.
+     */
+    function getNextRedemptionTime()
+        external
+        view
+        returns (uint256 nextRedemptionTime)
+    {
+        uint256 length = redemptions.length;
+        require(length > 0);
+        nextRedemptionTime = redemptions[length - 1].redemptionTime;
+    }
+
+    /**
+     * @notice Returns if current redemption's registration period is open.
+     *
+     * @return registrationOpen If registration period is open.
+     */
+    function isRegistrationOpen()
+        external
+        view
+        returns (bool registrationOpen)
+    {
+        uint256 length = redemptions.length;
+        if (length == 0) return false;
+        if (block.timestamp > redemptions[length - 1].registrationEndTime) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- contracts/interfaces/IRedemptionHelper.sol
+++ contracts/interfaces/IRedemptionHelper.sol
@@ -52,16 +52,6 @@ interface IRedemptionHelper {
         IERC20 token
     );
 
-    /* ============ External Functions ============ */
-
-    function initialize(address owner_, address admin_) external;
-
-    function redeem(uint256[] memory claims) external;
-
-    function register(uint256 amount) external;
-
-    function unregister(uint256 amount) external;
-
     /* ============ External Admin Functions ============ */
 
     /**
@@ -73,15 +63,163 @@ interface IRedemptionHelper {
 
     /* ============ External Owner Functions ============ */
 
+    /**
+     * @notice Set default interval between redemptions.
+     *
+     * @dev It can be overriden by owner on 'activateRedemption'.
+     *
+     * @param interval New redemption interval (in seconds).
+     */
     function setRedemptionInterval(uint256 interval) external;
 
+    /**
+     * @notice Set preparation time for next redemption.
+     *
+     * @dev Preparation time allows users to register/unregister.
+     *
+     * @param preparationTime New preparation time (in seconds).
+     */
     function setPreparationTime(uint256 preparationTime) external;
 
+    /**
+     * @notice Set redemption token.
+     *
+     * @dev Current contract supports only stablecoins.
+     *
+     * @param token Address of the redemption token.
+     */
+    function setRedemptionToken(IERC20 token) external;
+
+    /**
+     * @notice Create first redemption.
+     *
+     * @dev It can only be called once, before any redemptions.
+     * @dev 'nextRedemptionExactTime' greater than 0 ignores '_redemptionInterval'.
+     *
+     * @param nextRedemptionExactTime Exact time of the new redemption (ignored if 0).
+     * @param fee Fee for the new redemption (max 1000 = 10%).
+     */
     function initializeRedemptions(uint256 nextRedemptionExactTime, uint256 fee)
         external;
 
+    /**
+     * @notice Activate redemption, burn registered vault tokens, transfer redemption tokens
+     * from owner to the contract, allow users to redeem.
+     *
+     * @dev Calling this function will create next redemption object.
+     * @dev 'nextRedemptionExactTime' greater than 0 ignores '_redemptionInterval'.
+     *
+     * @param nextRedemptionExactTime Exact time of the new redemption (ignored if 0).
+     * @param fee Fee for the new redemption (max 1000 = 10%).
+     */
     function activateRedemption(uint256 nextRedemptionExactTime, uint256 fee)
         external;
 
+    /* ============ External Functions ============ */
+
+    /**
+     * @notice Initializer function called by factory.
+     */
+    function initialize(address owner_, address admin_) external;
+
+    /**
+     * @notice Reedem redemption tokens.
+     *
+     * @param claims List of redemption indexes to claim from.
+     */
+    function redeem(uint256[] memory claims) external;
+
+    /**
+     * @notice Register vault tokens for the current redemption.
+     *
+     * @dev Only works if current redemtpion 'registrationTimeEnd' hasn't passed yet.
+     *
+     * @param amount Amount of vault tokens to register.
+     */
+    function register(uint256 amount) external;
+
+    /**
+     * @notice Unegister vault tokens for the current redemption.
+     *
+     * @dev Only works if current redemtpion 'registrationTimeEnd' hasn't passed yet.
+     *
+     * @param amount Amount of vault tokens to register.
+     */
+    function unregister(uint256 amount) external;
+
     /* ============ External View Functions ============ */
+
+    /**
+     * @notice Returns amount of vault tokens registered by the user for given redemption.
+     *
+     * @param _user Address of the vault user.
+     * @param _redemptionIndex Index of the redemption.
+     *
+     * @return amount Amount of tokens registered by the user.
+     * @return token Address of the redemption token.
+     */
+    function getRegisteredAmount(address _user, uint256 _redemptionIndex)
+        external
+        view
+        returns (uint256 amount, IERC20 token);
+
+    /**
+     * @notice Returns number of redemptions in the list.
+     *
+     * @return length Number of redemptions (length of redemptions list).
+     */
+    function getRedemptionsLength() external view returns (uint256 length);
+
+    /**
+     * @notice Returns all information of the given redemption.
+     *
+     * @param _redemptionIndex Index of the redemption.
+     *
+     * @return redemptionTime Time after which redemption can be activated.
+     * @return registrationEndTime Time until users can (un)register their tokens.
+     * @return pending Pending tokens to redeem.
+     * @return price Redemtpion value of tokens in 'token' after 'fee'.
+     * @return fee Fee taken (max 10%).
+     * @return token Token address used for redemption.
+     * @return active If users can redeem.
+     */
+    function getRedemption(uint256 _redemptionIndex)
+        external
+        view
+        returns (
+            uint256 redemptionTime,
+            uint256 registrationEndTime,
+            uint256 pending,
+            uint256 price,
+            uint256 fee,
+            IERC20 token,
+            bool active
+        );
+
+    /**
+     * @notice Returns next redemption fee.
+     *
+     * @return nextRedemptionFee Next redemption fee.
+     */
+    function getNextRedemptionFee()
+        external
+        view
+        returns (uint256 nextRedemptionFee);
+
+    /**
+     * @notice Returns next redemption time.
+     *
+     * @return nextRedemptionTime Time after which redemption can be activated.
+     */
+    function getNextRedemptionTime()
+        external
+        view
+        returns (uint256 nextRedemptionTime);
+
+    /**
+     * @notice Returns if current redemption's registration period is open.
+     *
+     * @return registrationOpen If registration period is open.
+     */
+    function isRegistrationOpen() external view returns (bool registrationOpen);
 }
